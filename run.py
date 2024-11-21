42baa3a08558483442262491dd2a300b26d2130d
Your Name <your-email@example.com>
2024-11-21 17:47:27 +0500

    add

--- run.py
+++ run.py
@@ -1,7 +1,11 @@
+# run.py
 from app import create_app
 
 app = create_app()
 
-# Remove the debug flag; Gunicorn manages it
 if __name__ == '__main__':
-    app.run()
+    try:
+        from waitress import serve
+        serve(app, host='127.0.0.1', port=8080)
+    except ImportError:
+        app.run(debug=True)
